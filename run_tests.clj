4c75ca22363ffbab192fac3a9aa576a6deccd215
Jun Kawasaki <jun784@gmail.com>
2026-07-17 22:32:52 +0900

    refactor(metadata): make EDN canonical for gtin

---
+++ run_tests.clj
@@ -0,0 +1,15 @@
+#!/usr/bin/env bb
+(require '[babashka.classpath :as cp]
+         '[babashka.fs :as fs]
+         '[clojure.test :as t])
+
+(let [root (fs/parent (fs/absolutize *file*))]
+  (cp/add-classpath (str root "/src"))
+  (cp/add-classpath (str root "/test")))
+
+(def suites '[gtin.murakumo-test
+              gtin.mesh-manifest-test
+              gtin.repository-contract-test])
+(apply require suites)
+(let [{:keys [fail error]} (apply t/run-tests suites)]
+  (System/exit (if (zero? (+ fail error)) 0 1)))
